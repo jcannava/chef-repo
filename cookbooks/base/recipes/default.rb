2b22b0ed70052b1a7b82b5fe5254aa67c7713a35
Jason Cannavale <jason@cannavale.com>
2012-01-24 11:17:16 -0600

    cleaning up

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -6,3 +6,87 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "mlocate" do
+ action :install
+end
+
+package "vim" do
+ action :install
+end
+
+package "git-core" do
+ action :install
+end
+
+package "ntp" do
+    action :install
+end
+
+template "/etc/ntp.conf" do
+    source "ntp.conf.erb"
+    variables(:ntp_server => "time.nist.gov")
+end
+
+service "ntpd" do
+    action[:enable,:start]
+end
+
+sysadmins_group = Array.new
+
+search(:users, 'groups:sysadmins') do |u|
+    sysadmins_group << u['id']
+    home_dir = "/home/#{u['id']}"
+
+    ruby_block "reset group list" do
+        block do
+            Etc.endgrent
+        end
+        action :nothing
+    end
+
+    user u['id'] do
+        uid u['uid']
+        gid u['gid']
+        shell u['shell']
+        comment u['comment']
+        supports :manage_home => true
+        home home_dir
+        notifies :create, "ruby_block[reset group list]", :immediately
+    end
+
+    directory "#{home_dir}/.ssh" do
+         owner u['id']
+         group u['gid'] || u['id']
+    end
+
+    template "#{home_dir}/.ssh/authorized_keys" do
+        source "authorized_keys.erb"
+        owner u['id']
+        group u['gid'] || u['id']
+        mode "0600"
+        variables :ssh_keys => u['ssh-keys']
+    end
+
+    template "#{home_dir}/.vimrc" do
+    	source "vimrc.erb"
+        owner u['id']
+        group u['gid'] || u['id']
+    end
+
+    template "#{home_dir}/.bash_profile" do
+    	source "bash_profile.erb"
+        owner u['id']
+        group u['gid'] || u['id']
+    end
+
+
+    template "/etc/sudoers" do
+        source "sudoers.erb"
+        mode "0440"
+    end
+end
+
+group "sysadmins" do
+    gid 501
+    members sysadmins_group
+end
